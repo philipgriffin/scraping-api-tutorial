ed4c8326a81e33cc5e0ea9960bc02cde916db1f9
Phil <philipgriffin88@gmail.com>
2019-05-28 21:41:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,3 @@ app.get("/philipgriffin/title", function(req, res) {
     }
   })();
 });
-
-
-
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
