6e5c7e81e4866a1053d09c2f2c675a8620c0283b
Phil <philipgriffin88@gmail.com>
2021-03-22 08:12:31 +0000

    finally added

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const puppeteer = require("puppeteer");
 
 app.listen(3000);
 
-app.get("/philipgriffin/title", function(req, res) {
-  void (async() => {
+app.get("/philipgriffin/title", function (req, res) {
+  void (async () => {
     const browser = await puppeteer.launch({ headless: true });
     try {
       const page = await browser.newPage();
       await page.goto("https://philip-griffin.com");
       const postTitleElement = await page.$(".post-title");
       const postTitle = await page.evaluate(postTitleElement => postTitleElement.textContent.trim(), postTitleElement);
-      await browser.close();
       res.status(200).json({ postTitle: postTitle });
     } catch (e) {
-      await browser.close();
       res.status(500).json({ error: "error" });
+    } finally {
+      await browser.close();
     }
   })();
 });
